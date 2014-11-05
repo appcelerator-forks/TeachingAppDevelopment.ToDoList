f10f55ad1a8385ddc41d2562bc01b6f13f02aafe
Claus Stilborg <cb@nebulators.com>
2014-11-05 18:51:54 +0100

    Labels TextField and Picker widgets added to the additem controller, picker initiated through the Window onOpen event handler

--- app/controllers/additem.js
+++ app/controllers/additem.js
@@ -1,1 +1,15 @@
 var args = arguments[0] || {};
+
+function doPostLayout(e){
+	Ti.API.info("doPostLayout");
+	
+}
+
+function doInit(e){
+	// Initialize the date picker
+	var now = new Date();
+	$.duedate.setMinDate(now);
+	$.duedate.setValue(now);
+	now.setFullYear(now.getFullYear() + 10);
+	$.duedate.setMaxDate(now);
+}
